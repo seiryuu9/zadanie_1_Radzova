471d77a446d22d731518ae99762cf594dbd021c6
seiryuu <165938383+seiryuu9@users.noreply.github.com>
2025-03-07 14:29:35 +0100

    Add files via upload

---
+++ 2117_infinite_loop/2117_infinite_loop/parts/footer.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Infinite Loop - Bootstrap 4.0 HTML Template</title>
+    <link rel="stylesheet" href="fontawesome-5.5/css/all.min.css" />
+    <link rel="stylesheet" href="slick/slick.css">
+    <link rel="stylesheet" href="slick/slick-theme.css">
+    <link rel="stylesheet" href="magnific-popup/magnific-popup.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/tooplate-infinite-loop.css" />
+
+</head>
+
+
+<section id="contact" class="tm-section-pad-top tm-parallax-2">
+
+    <div class="container tm-container-contact">
+    <div class="container tm-container-contact">
+
+        <div class="row">
+
+            <div class="text-center col-12">
+                <h2 class="tm-section-title mb-4">Contact Us</h2>
+                <p class="mb-5">
+                    Proin enim orci, tincidunt quis suscipit in, placerat nec est. Vestibulum posuere faucibus posuere. Quisque aliquam velit eget leo blandit egestas. Nulla id posuere felis, quis tristique nulla.
+                </p><br>
+            </div>
+
+            <div class="col-sm-12 col-md-6">
+                <form action="" method="get">
+                    <input id="name" name="name" type="text" placeholder="Your Name" class="tm-input" required />
+                    <input id="email" name="email" type="email" placeholder="Your Email" class="tm-input" required />
+                    <textarea id="message" name="message" rows="8" placeholder="Message" class="tm-input" required></textarea>
+                    <button type="submit" class="btn tm-btn-submit">Submit</button>
+                </form>
+            </div>
+
+            <div class="col-sm-12 col-md-6">
+
+                <div class="contact-item">
+                    <a rel="nofollow" href="https://www.tooplate.com/contact" class="item-link">
+                        <i class="far fa-2x fa-comment mr-4"></i>
+                        <span class="mb-0">Chat Online</span>
+                    </a>
+                </div>
+
+                <div class="contact-item">
+                    <a rel="nofollow" href="mailto:mail@company.com" class="item-link">
+                        <i class="far fa-2x fa-envelope mr-4"></i>
+                        <span class="mb-0">mail@company.com</span>
+                    </a>
+                </div>
+
+                <div class="contact-item">
+                    <a rel="nofollow" href="https://www.google.com/maps" class="item-link">
+                        <i class="fas fa-2x fa-map-marker-alt mr-4"></i>
+                        <span class="mb-0">Our Location</span>
+                    </a>
+                </div>
+
+                <div class="contact-item">
+                    <a rel="nofollow" href="tel:0100200340" class="item-link">
+                        <i class="fas fa-2x fa-phone-square mr-4"></i>
+                        <span class="mb-0">255-662-5566</span>
+                    </a>
+                </div>
+
+                <div class="contact-item">&nbsp;</div>
+
+            </div>
+
+
+        </div><!-- row ending -->
+
+    </div>
+
+    <footer class="text-center small tm-footer">
+        <p class="mb-0">
+            Copyright &copy; 2020 Company Name
+
+            . <a rel="nofollow" href="https://www.tooplate.com" title="HTML templates">Designed by TOOPLATE</a></p>
+    </footer>
+
+</section>
---
+++ 2117_infinite_loop/2117_infinite_loop/parts/header.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Infinite Loop - Bootstrap 4.0 HTML Template</title>
+    <link rel="stylesheet" href="fontawesome-5.5/css/all.min.css" />
+    <link rel="stylesheet" href="slick/slick.css">
+    <link rel="stylesheet" href="slick/slick-theme.css">
+    <link rel="stylesheet" href="magnific-popup/magnific-popup.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/tooplate-infinite-loop.css" />
+
+</head>
+
+
+<section id="infinite" class="text-white tm-font-big tm-parallax">
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-md tm-navbar" id="tmNav">
+        <div class="container">
+            <div class="tm-next">
+                <a href="#infinite" class="navbar-brand">Infinite Loop</a>
+            </div>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-bars navbar-toggler-icon"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link tm-nav-link" href="#infinite">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link tm-nav-link" href="#whatwedo">What We Do</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link tm-nav-link" href="#testimonials">Testimonials</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link tm-nav-link" href="#gallery">Gallery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link tm-nav-link" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="text-center tm-hero-text-container">
+        <div class="tm-hero-text-container-inner">
+            <h2 class="tm-hero-title">Infinite Loop</h2>
+            <p class="tm-hero-subtitle">
+                Bootstrap 4.0 Parallax Theme
+                <br>Free HTML Template by TOOPLATE
+            </p>
+        </div>
+    </div>
+
+    <div class="tm-next tm-intro-next">
+        <a href="#whatwedo" class="text-center tm-down-arrow-link">
+            <i class="fas fa-2x fa-arrow-down tm-down-arrow"></i>
+        </a>
+    </div>
+</section>
