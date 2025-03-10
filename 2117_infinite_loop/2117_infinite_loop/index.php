2d523fe0eb2b0c4753f3e285757432b6456adfaf
seiryuu <draxasyt@gmail.com>
2025-03-10 20:54:43 +0100

    funkcia

--- 2117_infinite_loop/2117_infinite_loop/index.php
+++ 2117_infinite_loop/2117_infinite_loop/index.php
@@ -152,33 +152,8 @@ https://www.tooplate.com/view/2117-infinite-loop
     </section>
 
     <?php
-
-    $jsonData = file_get_contents('../2117_infinite_loop/parts/galeria.json');
-
-    $gallery = json_decode($jsonData, true);
-
-        $imagesCount = count($gallery['images']);
-        ?>
-        <section id="gallery" class="tm-gallery-section">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12">
-                        <h2 class="tm-section-title">Our Gallery</h2>
-                    </div>
-                </div>
-                <div class="row tm-gallery">
-                    <?php for ($i = 0; $i < $imagesCount; $i++): ?>
-                        <div class="col-sm-12 col-md-6 col-lg-3">
-                            <div class="tm-gallery-item">
-                                <a href="<?php echo htmlspecialchars($gallery['images'][$i]['src']); ?>" target="_blank">
-                                    <img src="<?php echo htmlspecialchars($gallery['images'][$i]['src']); ?>" alt="<?php echo htmlspecialchars($gallery['images'][$i]['alt']); ?>" class="img-fluid">
-                                </a>
-                            </div>
-                        </div>
-                    <?php endfor; ?>
-                </div>
-            </div>
-        </section>
+        include_once "../2117_infinite_loop/parts/galeriaFunkcia.php"
+    ?>
 
 
     <!-- Contact -->
